ca4f560261f1d4a64471a2c672e58ed2bc5ef93d
moonboot333 <monobootica@yandex.ru>
2024-06-10 09:35:53 +0500

    fix routes;

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::name('front.')
     ->group(function () {
         Route::get('/', [MainController::class, 'index'])->name('main');
-        Route::post('/send', [MainController::class, 'send'])->name('send');
     });
 
 //Сброс кэша
